b2b23db413716f0f8a169ad71b940375c396b1a9
lucaspaessptech <lucas.paes@sptech.school>
2024-11-28 10:45:34 -0300

    Criptografia md5 adicionada ao campo senha

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -3,7 +3,7 @@ var database = require("../database/config")
 function autenticar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
     var instrucaoSql = `
-        SELECT email, senha, fkEmpresa, nome, idRepresentante FROM Usuario WHERE email = '${email}' AND senha = '${senha}';`;
+        SELECT email, senha, fkEmpresa, nome, idRepresentante FROM Usuario WHERE email = '${email}' AND senha = md5('${senha}');`;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
@@ -15,7 +15,7 @@ function cadastrar(nome, email, senha, telefone, cpf, cargo, fkEmpresa) {
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucaoSql = `
-       INSERT INTO usuario (nome, status_colaborador, email, senha, telefone, cpf, cargo, fkEmpresa) VALUES ('${nome}', ${1}, '${email}', '${senha}', '${telefone}', '${cpf}', '${cargo}', ${fkEmpresa});
+       INSERT INTO usuario (nome, status_colaborador, email, senha, telefone, cpf, cargo, fkEmpresa) VALUES ('${nome}', ${1}, '${email}', md5('${senha}'), '${telefone}', '${cpf}', '${cargo}', ${fkEmpresa});
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);

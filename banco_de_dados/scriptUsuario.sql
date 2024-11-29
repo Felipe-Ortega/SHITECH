3669c23fcafaf94e5646f5fdbf7afa684d6ef06d
amandampedroza <amanda.pedroza@sptech.school>
2024-11-29 18:10:05 -0300

    Formatação do script de criação de usuário

--- banco_de_dados/scriptUsuario.sql
+++ banco_de_dados/scriptUsuario.sql
@@ -1,9 +1,9 @@
 create user 'shitech'@'%' identified by 'Shitech#123';
 
-grant all on shitech.* to 'shitech'@'%';
+grant all on Shitech.* to 'shitech'@'%';
 
 create user 'shitechINSERT'@'%' identified by 'Shitech#123';
 
-grant insert on shitech.Dados to 'shitechINSERT'@'%';
+grant insert on Shitech.Dados to 'shitechINSERT'@'%';
 
 flush privileges;

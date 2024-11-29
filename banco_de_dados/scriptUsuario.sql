85415e25df20bba099e0212189ea09682cacb545
amandampedroza <amanda.pedroza@sptech.school>
2024-11-29 17:56:28 -0300

    Criação de conta responsável por INSERT no banco de dados

--- banco_de_dados/scriptUsuario.sql
+++ banco_de_dados/scriptUsuario.sql
@@ -2,4 +2,8 @@ create user 'shitech'@'%' identified by 'Shitech#123';
 
 grant all on shitech.* to 'shitech'@'%';
 
+create user 'shitechINSERT'@'%' identified by 'Shitech#123';
+
+grant insert on shitech.Dados to 'shitechINSERT'@'%';
+
 flush privileges;

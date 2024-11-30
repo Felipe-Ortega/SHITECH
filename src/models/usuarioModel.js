b63054c98265e3e2fbd93896a891e6c4f64fa00b
lucaspaessptech <lucas.paes@sptech.school>
2024-11-29 22:57:37 -0300

    Formatação do model de usuario

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -4,7 +4,7 @@ var database = require("../database/config")
 function autenticar(email, senha) {
    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
    var instrucaoSql = `
-       SELECT email, senha, fkEmpresa, nome, idrepresentante FROM Usuario WHERE Email = '${email}' AND Senha = MD5('${senha}');`;
+       SELECT email, senha, fkEmpresa, nome, idRepresentante FROM Usuario WHERE Email = '${email}' AND Senha = MD5('${senha}');`;
    console.log("Executando a instrução SQL: \n" + instrucaoSql);
    return database.executar(instrucaoSql);
 }
